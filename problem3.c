be8e2a4a9a11d858f127f216c80f6c7c82ea65b0
Prashant kumar <prashant.kumar2025@glbajaj.group.org>
2026-06-07 16:17:29 +0530

    first commit

---
+++ problem3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    int n,i;
+    long long fact =1;
+    printf("enter a number");
+    scanf("%d" , &n);
+    for(i=1;i<=n;i++) {
+        fact*=i;
+    }
+    printf("factorial = %lld",fact);
+    return 0;
+}
---
+++ problem4.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(){
+    int n,count=0;
+
+    printf("enter a number");
+    scanf("%d",&n);
+    if(n==0) {
+        count =1;
+    }
+    else{
+        while(n!=0) {
+            count++;
+            n/=10;
+        }
+    }
+    printf("number of digits = %d" , count);
+    return 0;
+}
