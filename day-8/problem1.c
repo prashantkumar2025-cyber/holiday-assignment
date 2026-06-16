7440b40a749276f749788b54021f7e87a695ce36
Prashant kumar <prashant.kumar2025@glbajaj.group.org>
2026-06-16 19:05:45 +0530

    day-8 complete

---
+++ day-8/problem1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+    int n,i,j;
+
+    printf("enter number of rows \n");
+    scanf("%d" , &n);
+
+    for(i=1;i<=n;i++) {
+        for(j=1;j<=i;j++) {
+            printf("* ");
+            
+        }
+        printf(" \n");
+        
+    }
+    return 0;
+}
