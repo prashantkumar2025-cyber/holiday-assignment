fc098412b70298db4a0007b8adae5528ef31432b
Prashant kumar <prashant.kumar2025@glbajaj.group.org>
2026-06-26 18:43:54 +0530

    day-22 day-23 complete

---
+++ day-23/problem3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str1[100], str2[100];
+    int i, count1[256] = {0}, count2[256] = {0};
+
+    printf("Enter first string: ");
+    gets(str1);
+
+    printf("Enter second string: ");
+    gets(str2);
+
+    if(strlen(str1) != strlen(str2)) {
+        printf("Not Anagram");
+        return 0;
+    }
+
+    for(i = 0; str1[i] != '\0'; i++) {
+        count1[(unsigned char)str1[i]]++;
+        count2[(unsigned char)str2[i]]++;
+    }
+
+    for(i = 0; i < 256; i++) {
+        if(count1[i] != count2[i]) {
+            printf("Not Anagram");
+            return 0;
+        }
+    }
+
+    printf("Anagram");
+
+    return 0;
+}
